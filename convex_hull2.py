fc1c6dab1453ddb10676701691c6d49c6ef526ac
dustintwatkins <dustintwatkins@gmail.com>
2018-02-08 19:26:31 -0700

    debug

--- convex_hull2.py
+++ convex_hull2.py
@@ -19,13 +19,13 @@ def convex_hull(sorted_points):
         return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
 
     lower = []
-    for p in points:
+    for p in sorted_points:
         while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
             lower.pop()
         lower.append(p)
 
     upper = []
-    for p in reversed(points):
+    for p in reversed(sorted_points):
         while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
             upper.pop()
         upper.append(p)
